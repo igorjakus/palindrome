6a0ac81705a3927959c3aef4cb3f955082ca21ee
igorjakus <igorjakus3@gmail.com>
2020-09-14 16:52:30 +0200

    Better is_palindrome using: - one function operates both int and str - raising error when argument type is not int or str

--- palindrome.py
+++ palindrome.py
@@ -1,16 +1,20 @@
-def is_text_palindrome(text):
-    text = text.lower().replace(' ', '')
-    return text == text[::-1]
+def is_palindrome(var):
 
-def is_number_palindrome(number):
-    num_list = []
-    while number:
-        num_list.append(number % 10)
-        number = number // 10
-    return num_list == num_list[::-1]
+    if isinstance(var, str):
+        var = var.lower().replace(' ', '')
+        return var == var[::-1]
 
-    # 1234 -> [1, 2, 3, 4]
-    # 1234 % 10 = 4
-    # 123 % 10 = 3
-    # 12 % 10 = 2
-    # 1 % 10 = 1
+    elif isinstance(var, int) and not isinstance(var, bool):
+        num_list = []
+        while var:
+            num_list.append(var % 10)
+            var = var // 10
+        return num_list == num_list[::-1]
+        # 1234 -> [4, 3, 2, 1]
+        # 1234 % 10 = 4
+        # 123 % 10 = 3
+        # 12 % 10 = 2
+        # 1 % 10 = 1
+
+    # When var type isn't str or int
+    raise TypeError
