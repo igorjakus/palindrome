ea9c40a193aec32fc4332872ee39ae8ea1da0c92
igorjakus <igorjakus3@gmail.com>
2020-09-14 16:50:09 +0200

    Better unittest using

--- tests.py
+++ tests.py
@@ -1,33 +1,35 @@
-from palindrome import is_text_palindrome, is_number_palindrome
+from palindrome import is_palindrome
 import unittest
 
 class TestPalindrome(unittest.TestCase):
 
-    def test_text_palindrome(self):
-        
-        self.assertTrue(is_text_palindrome("atok am ala ala ma kota"))
-        self.assertTrue(is_text_palindrome("Kobyła ma mały bok"))
-        self.assertTrue(is_text_palindrome("a b c cb a"))
-        self.assertTrue(is_text_palindrome("aDa"))
-        self.assertTrue(is_text_palindrome(" _"))
-        
-        self.assertFalse(is_text_palindrome("loret ipsum 000 _"))
-        self.assertFalse(is_text_palindrome("dfejfwehg5ujgrs"))
-        self.assertFalse(is_text_palindrome("Ala ma kota"))
-        self.assertFalse(is_text_palindrome("45t45"))
+    def test_text_palindrome_1(self):
+        self.assertTrue(is_palindrome('level'))
 
-    def test_number_palindrome(self):
-        self.assertTrue(is_number_palindrome(18518509568600686590581581))
-        self.assertTrue(is_number_palindrome(5812351532185))
-        self.assertTrue(is_number_palindrome(1234554321))
-        self.assertTrue(is_number_palindrome(2222222))
-        self.assertTrue(is_number_palindrome(1537351))
+    def test_text_palindrome_2(self):
+        self.assertTrue(is_palindrome('asflaksl5748w55w8475lskalfsa'))
 
-        self.assertFalse(is_number_palindrome(11111111111115111111))
-        self.assertFalse(is_number_palindrome(1243456788765321))
-        self.assertFalse(is_number_palindrome(541414155168411))
-        self.assertFalse(is_number_palindrome(54411))
-        
+    def test_text_not_palindrome(self):
+        self.assertFalse(is_palindrome('qwerty'))
+
+    def test_number_palindrome_1(self):
+        self.assertTrue(is_palindrome(12321))
+
+    def test_number_palindrome_2(self):
+        self.assertTrue(is_palindrome(5185095686006865905815))
+
+    def test_number_not_palindrome(self):
+        self.assertFalse(is_palindrome(12345))
+
+    def test_wrong_type_1(self):
+        self.assertRaises(TypeError, is_palindrome, 5.0)
+
+    def test_wrong_type_2(self):
+        self.assertRaises(TypeError, is_palindrome, True)
+    
+    def test_wrong_type_3(self):
+        self.assertRaises(TypeError, is_palindrome, None)
+    
 
 if __name__ == '__main__':
     unittest.main()
